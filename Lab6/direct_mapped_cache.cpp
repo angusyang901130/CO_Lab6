5023d03c6236302b85e6d25f9a0f842f675140ba
username <email>
2022-06-05 23:51:43 +0800

    done: both table and debug

--- Lab6/direct_mapped_cache.cpp
+++ Lab6/direct_mapped_cache.cpp
@@ -1,18 +1,9 @@
 #include "direct_mapped_cache.h"
+#include "operation.h"
 #include "string"
 #include <vector>
 #include <fstream>
-#include <bitset>
-#include <sstream>
 #include <set>
-
-using namespace std;
-int bitnum(int);
-string hex2bin(char);
-int bin2dec(string);
-string dec2bin(int);
-bool comparer(string, string);
-string padding(string);
 
 float direct_mapped(string filename, int block_size, int cache_size){
 
@@ -59,82 +50,6 @@ float direct_mapped(string filename, int block_size, int cache_size){
     }
 
     file.close();
-    cout << total_num << endl;
   
     return (float)hit_num/total_num;
-}
-
-int bitnum(int num){
-    int power = -1;
-    while(num != 0){
-        power += 1;
-        num /= 2;
-    }
-
-    return power;
-}
-
-string hex2bin(char c){
-    switch(c){
-        case '0': return "0000";
-        case '1': return "0001";
-        case '2': return "0010";
-        case '3': return "0011";
-        case '4': return "0100";
-        case '5': return "0101";
-        case '6': return "0110";
-        case '7': return "0111";
-        case '8': return "1000";
-        case '9': return "1001";
-        case 'a': return "1010";
-        case 'b': return "1011";
-        case 'c': return "1100";
-        case 'd': return "1101";
-        case 'e': return "1110";
-        case 'f': return "1111";
-    }
-}
-
-int bin2dec(string bin){
-    int base = 1;
-    int dec = 0;
-    for(int i = bin.size()-1; i >= 0; i--){
-        dec += (bin[i] == '1') * base;
-        base *= 2;
-    }
-    return dec;
-}
-
-string dec2bin(int num){
-    string bin = "";
-    while(num != 0){
-        int mod = num % 2;
-        num /= 2;
-        if(mod)
-            bin = "1" + bin;
-        else 
-            bin = "0" + bin;
-    }
-    return bin;
-}
-
-bool comparer(string s1, string s2){
-    if(s1.size() != s2.size())
-        return false;
-    else{
-        for(int i = 0; i < s1.size(); i++){
-            if(s1[i] != s2[i])
-                return false;
-        }
-        return true;
-    }
-}
-
-string padding(string str){
-    int len = str.size();
-    int pad_len = 32/4 - len;
-    for(int i = 0; i < pad_len; i++){
-        str = '0' + str;
-    }
-    return str;
 }
--- Lab6/set_associative_cache.cpp
+++ Lab6/set_associative_cache.cpp
@@ -1,19 +1,9 @@
 #include "set_associative_cache.h"
+#include "operation.h"
 #include "string"
 #include <vector>
 #include <fstream>
-#include <bitset>
-#include <sstream>
 #include <set>
-
-using namespace std;
-int bitnum(int);
-string hex2bin(char);
-int bin2dec(string);
-string dec2bin(int);
-bool comparer(string, string);
-string padding(string);
-
 
 float set_associative(string filename, int way, int block_size, int cache_size){
     int total_num = 0;
@@ -22,11 +12,13 @@ float set_associative(string filename, int way, int block_size, int cache_size){
     /*write your code HERE*/
     // set up cache table
     int block_num = cache_size / block_size;
+    //cout << "block_num" << block_num << endl;
     int row_num = block_num / way;
     
     int tag_bit = 32 - bitnum(row_num) - bitnum(block_size);
     
-    vector< vector<string> > cache_tag(row_num, vector<string>(way));
+    
+    vector< vector<string> > cache_tag(row_num, vector<string>(way, ""));
     vector< vector<bool> > valid(row_num, vector<bool>(way, false));
     vector<string> used_order;
     
@@ -34,12 +26,15 @@ float set_associative(string filename, int way, int block_size, int cache_size){
     ifstream file;
     file.open(filename);
     string addr;
+
     while(file >> addr){
         total_num += 1;
         
+        //cout << total_num << endl;
+
         if(addr.size() < 32/4)
             addr = padding(addr);
-
+        
         // address transfer hex to bin
         string addr_bin = "";
         for(int i = 0; i < addr.size(); i++){
@@ -74,22 +69,22 @@ float set_associative(string filename, int way, int block_size, int cache_size){
             
             // put stored tag of cache_tag[row_index] into set 
             for(int i = 0; i < way; i++)
-                st.insert(cache_tag[row_index][i]);
+                st.insert(cache_tag[row_index][i] + addr_bin.substr(tag_bit, bitnum(row_num)));
 
             // search in used_order, if found one used is stored in set, erase it until set size = 1 
             // if set size == 1, means only one tag left, it is the least recently used tag
             while(st.size() > 1){
                 set<string>::iterator pos_iter = st.find(*ptr);
-
+                
                 if(pos_iter != st.end())
                     st.erase(pos_iter);
 
-                else ptr--;
+                ptr--;
             }
 
             // replace the tag with new tag
             for(int i = 0; i < way; i++){
-                if(cache_tag[row_index][i] == *st.begin()){
+                if(cache_tag[row_index][i] == (*st.begin()).substr(0, tag_bit)){
                     cache_tag[row_index][i] = tag;
                     break;
                 }
@@ -101,82 +96,6 @@ float set_associative(string filename, int way, int block_size, int cache_size){
     }
 
     file.close();
-    cout << total_num << endl;
-    
+
     return (float)hit_num/total_num;
-}
-
-int bitnum(int num){
-    int power = -1;
-    while(num != 0){
-        power += 1;
-        num /= 2;
-    }
-
-    return power;
-}
-
-string hex2bin(char c){
-    switch(c){
-        case '0': return "0000";
-        case '1': return "0001";
-        case '2': return "0010";
-        case '3': return "0011";
-        case '4': return "0100";
-        case '5': return "0101";
-        case '6': return "0110";
-        case '7': return "0111";
-        case '8': return "1000";
-        case '9': return "1001";
-        case 'a': return "1010";
-        case 'b': return "1011";
-        case 'c': return "1100";
-        case 'd': return "1101";
-        case 'e': return "1110";
-        case 'f': return "1111";
-    }
-}
-
-int bin2dec(string bin){
-    int base = 1;
-    int dec = 0;
-    for(int i = bin.size()-1; i >= 0; i--){
-        dec += (bin[i] == '1') * base;
-        base *= 2;
-    }
-    return dec;
-}
-
-string dec2bin(int num){
-    string bin = "";
-    while(num != 0){
-        int mod = num % 2;
-        num /= 2;
-        if(mod)
-            bin = "1" + bin;
-        else 
-            bin = "0" + bin;
-    }
-    return bin;
-}
-
-bool comparer(string s1, string s2){
-    if(s1.size() != s2.size())
-        return false;
-    else{
-        for(int i = 0; i < s1.size(); i++){
-            if(s1[i] != s2[i])
-                return false;
-        }
-        return true;
-    }
-}
-
-string padding(string str){
-    int len = str.size();
-    int pad_len = 32/4 - len;
-    for(int i = 0; i < pad_len; i++){
-        str = '0' + str;
-    }
-    return str;
 }
